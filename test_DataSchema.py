97248e976f6a8bdf27ccc5d9db05bd50ec6dbf5b
Guy McFall <guymcfall@gmail.com>
2025-02-23 13:56:29 -0600

    Added in code comments to the Data Schema test

--- test_DataSchema.py
+++ test_DataSchema.py
@@ -3,6 +3,9 @@ import pandas as pd
 import json
 import numpy as np
 
+#Create testing data for 'ProcessorID', 'TaskName', and 'StationID' to test
+#functionality of the DataSchema script.
+
 task_data = {"ProcessorID": [101, 102, 103, 104],
     "TaskName": ["Task A", "Task B", "Task C", "Task D"]}
 
@@ -21,15 +24,23 @@ def Sort_Stations(df, dictionary, column_name: str, key_column: str):
 
 class TestSortStations(unittest.TestCase):
 
+#Test to determine if the function correctly maps stations to processors and tasks.
+#This test will not only test the function but also the effectiveness of the dictionary
+
     def test_correct_mapping(self):
         df_result = Sort_Stations(TaskProc_df.copy(), ProcStat_dict, "StationID", "ProcessorID")
         expected_stations = ["S1", "S2", "S3", None]  # ProcessorID 104 is missing in the dictionary
         self.assertListEqual(df_result["StationID"].tolist(), expected_stations)
 
+#Test to see if the function will be able to identify when a processor does not have a linked station.
+
     def test_missing_processor_id(self):
         df_result = Sort_Stations(TaskProc_df.copy(), ProcStat_dict, "StationID", "ProcessorID")
         self.assertIsNone(df_result.loc[df_result["ProcessorID"] == 104, "StationID"].values[0])
 
+#Test if the function will break when fed blank information for ProcessorID and TaskName.
+#The function should recognize it is blank and not produce an error.
+ 
     def test_empty_dataframe(self):
         empty_df = pd.DataFrame(columns=["ProcessorID", "TaskName"])
         df_result = Sort_Stations(empty_df, ProcStat_dict, "StationID", "ProcessorID")
